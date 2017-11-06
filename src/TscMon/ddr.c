f231ec80a4ea7a0a99c576c9db30d193532deb80
Xavier Paillard <xavier.paillard@ioxos.ch>
2017-11-06 11:47:17 +0100

    DDR3 calibration count-method [XP]

--- src/TscMon/ddr.c
+++ src/TscMon/ddr.c
@@ -49,6 +49,9 @@
 
 #include "ddr.h"
 
+// IDEL adjustment register for both DDR3 memory
+unsigned int SMEM_DDR3_IFSTA[2] = {0x808, 0xc0b};
+
 // IDEL control register for both DDR3 memory
 unsigned int SMEM_DDR3_IDEL[2] = {0x80c, 0xc0c};
 
@@ -173,6 +176,463 @@ int althea_ddr_idel_status(int mem){
 // DQ[07..0] -> DATA[15..8]
 // ----------------------------------------------------------------------------------
 int althea_ddr_idel_calib(int mem){
+	struct tsc_ioctl_map_win map_win;
+	char para_buf[32];
+	int  DQ_NOK[16];
+	float   f0, f1, f2	    		= 0.0;
+    int             retval          = 0;
+	unsigned int    *buf_ddr 		= NULL;	    // Buffer mapped directly in DDR3 area
+    unsigned int    *buf_tx  		= NULL;	    // Locally buffer to send data to DDR3
+    unsigned int    *buf_rx  		= NULL;	    // Locally buffer to receive data from DDR3
+    unsigned int    *buf_tx_start   = NULL;
+    unsigned int    size   		    = 0x40;
+    unsigned int    offset 		    = 0x100000; // DDR3 offset memory
+    unsigned int    d0, d1, d2	    = 0;
+    unsigned int    data            = 0;
+    unsigned int    init            = 0;
+    unsigned int    cnt_value  = 0;
+    unsigned int    init_cnt_value_store[16]  = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned int    temp_cnt_value_store[16]  = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned int    final_cnt_value_store[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned int    dq_path         = 0;
+
+    ////////////////////////////////////////////////////////////////////////////////////
+    // ADJUST DEFAULT VALUE FOR CURRENT_DLY ACCORDING TO HARDWARE IMPLEMENTATION      //
+    unsigned int    CURRENT_DLY     = 256;	   // Current delay value                 //
+    // ADJUST DEFAULT STEP VALUE FOR INC / DEC VALUE                                  //
+    unsigned int    CURRENT_STEP    = 8;	   // Current step value                  //
+    ////////////////////////////////////////////////////////////////////////////////////
+
+    unsigned int    MAX             = 0x1ff/*64*/;      // Max delay tap value
+    unsigned int    j, k, m, n      = 0;
+    unsigned int    start           = 0; 	   // Save the start index
+    unsigned int    end	            = 0; 	   // Save the end index
+    unsigned int    ok              = 0;	   // Count the number of passed test "1"
+    unsigned int    avg_x           = 0;       // Horizontal DQ average delay for 16 lines of DQ
+    unsigned int    marker          = 0;	   // Final delay marker value per line
+    unsigned int    NOK             = 1;	   // Calibration is done or not
+    unsigned int    pattern[32]     = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    unsigned int    ref_pattern[32] = {0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1};
+
+    // 32 bits reference pattern for each DQ : 0101 1001 0011 0100 1011 0101 1001 0011
+
+    unsigned int word0  = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+    unsigned int word1  = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+
+    unsigned int word2  = 0x0000ffff; // 0000 0000 0000 0000 1111 1111 1111 1111
+    unsigned int word3  = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+
+    unsigned int word4  = 0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
+    unsigned int word5  = 0xffffffff; // 1111 1111 1111 1111 1111 1111 1111 1111
+
+    unsigned int word6  = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+    unsigned int word7  = 0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
+
+    unsigned int word8  = 0x0000ffff; // 0000 0000 0000 0000 1111 1111 1111 1111
+    unsigned int word9  = 0xffffffff; // 1111 1111 1111 1111 1111 1111 1111 1111
+
+    unsigned int word10 = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+    unsigned int word11 = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+
+    unsigned int word12 = 0x0000ffff; // 0000 0000 0000 0000 1111 1111 1111 1111
+    unsigned int word13 = 0xffff0000; // 1111 1111 1111 1111 0000 0000 0000 0000
+
+    unsigned int word14 = 0x00000000; // 0000 0000 0000 0000 0000 0000 0000 0000
+    unsigned int word15 = 0xffffffff; // 1111 1111 1111 1111 1111 1111 1111 1111
+
+    // Buffer definition
+	buf_tx       = malloc(size);
+	buf_rx       = malloc(size);
+	buf_tx_start = buf_tx;
+
+	// Map DDR3 memory region --
+    buf_ddr = NULL;
+    memset(&map_win, sizeof(map_win), 0);
+    map_win.req.rem_addr   = offset;
+    map_win.req.loc_addr   = 0;
+	map_win.req.size       = size;
+	map_win.req.mode.sg_id = MAP_ID_MAS_PCIE_MEM;
+	if ((mem - 1) == 0){
+		map_win.req.mode.space = MAP_SPACE_SHM1; // SHM #1
+	}
+	else if((mem - 1) == 1){
+		map_win.req.mode.space = MAP_SPACE_SHM2; // SHM #2
+	}
+	map_win.req.mode.flags = 0;
+	retval = tsc_map_alloc(&map_win);
+	if(retval < 0){
+		printf("Error in mapping SHM");
+		return (-1);
+	}
+	buf_ddr = mmap(NULL, map_win.req.size, PROT_READ | PROT_WRITE, MAP_SHARED, tsc_fd, map_win.req.loc_addr);
+	if(buf_ddr == MAP_FAILED){
+	    printf("Error MAP FAILED \n");
+	    return (-1);
+	}
+
+    // Init local tx buffer with specific pattern
+    *buf_tx = word0;
+    buf_tx++;
+    *buf_tx = word1;
+    buf_tx++;
+    *buf_tx = word2;
+    buf_tx++;
+    *buf_tx = word3;
+    buf_tx++;
+    *buf_tx = word4;
+    buf_tx++;
+    *buf_tx = word5;
+    buf_tx++;
+    *buf_tx = word6;
+    buf_tx++;
+    *buf_tx = word7;
+    buf_tx++;
+    *buf_tx = word8;
+    buf_tx++;
+    *buf_tx = word9;
+    buf_tx++;
+    *buf_tx = word10;
+    buf_tx++;
+    *buf_tx = word11;
+    buf_tx++;
+    *buf_tx = word12;
+    buf_tx++;
+    *buf_tx = word13;
+    buf_tx++;
+    *buf_tx = word14;
+    buf_tx++;
+    *buf_tx = word15;
+
+    buf_tx = buf_tx_start;
+
+    // Acquire temperature and voltage of current system
+    d0 = 0x3000;
+    tsc_smon_write(0x41, &d0);
+    printf("   FPGA System Monitoring\n");
+    tsc_smon_read(0x00, &d0);
+    f0 = (((double)(d0 >> 6) * 503.975) / 1024.) - (double)273.15;
+    tsc_smon_read(0x20, &d1);
+    f1 = (((double)(d1 >> 6) * 503.975) / 1024.) - 273.15;
+    tsc_smon_read(0x24, &d2);
+    f2 = (((double)(d2 >> 6) * 503.975) / 1024.) - 273.15;
+    printf("      Temperature          : %.2f [%.2f - %.2f]\n", f0, f1, f2);
+    tsc_smon_read(0x01, &d0);
+    f0 = (((double)(d0 >> 6) * 3.0) / 1024.);
+    tsc_smon_read(0x21, &d1);
+    f1 = (((double)(d1 >> 6) * 3.0) / 1024.);
+    tsc_smon_read(0x25, &d2);
+    f2 = (((double)(d2 >> 6) * 3.0) / 1024.);
+    printf("      VCCint               : %.2f [%.2f - %.2f]\n", f0, f1, f2);
+    tsc_smon_read(0x02, &d0);
+    f0 = (((double)(d0 >> 6) * 3.0) / 1024.);
+    tsc_smon_read(0x22, &d1);
+    f1 = (((double)(d1 >> 6) * 3.0) / 1024.);
+    tsc_smon_read(0x26, &d2);
+    f2 = (((double)(d2 >> 6) * 3.0) / 1024.);
+    printf("      VCCaux               : %.2f [%.2f - %.2f]\n", f0, f1, f2);
+    d0 = 3;
+    tsc_smon_write(0x40, &d0);
+
+	printf("\n");
+	printf("Enter hardware default DQ delay (default value: %i): ", CURRENT_DLY);
+	gets(para_buf);
+	sscanf( para_buf, " %d", &CURRENT_DLY);
+	printf(" %i", CURRENT_DLY);
+
+	printf("\n");
+	printf("Enter hardware default INC / DEC STEP (default value: %i): ", CURRENT_STEP);
+	gets(para_buf);
+	sscanf( para_buf, " %d", &CURRENT_STEP);
+	printf(" %i", CURRENT_STEP);
+
+	printf("\n");
+    printf("Calibration pattern   : 0101 1001 0011 0100 1011 0101 1001 0011 \n");
+    printf("Default absolute IDEL : %i \n", CURRENT_DLY);
+    printf("Default STEP          : %d \n", CURRENT_STEP);
+    printf("\n");
+
+    printf("Tap = Delay x STEP\n");
+    printf("\n");
+
+    // Pass the entire possible delay taps
+    printf("+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+ \n");
+    printf(" Default delay :  ");
+    for(j = 0; j < MAX; j++){
+    	if(j == (CURRENT_DLY / CURRENT_STEP)){
+    		printf("*");
+    		break;
+    	}
+    	else{
+    		printf("   ");
+    	}
+    }
+    printf("\n");
+    printf(" Delay value   : 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 \n");
+    printf("+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+ \n");
+
+    // Set IDEL to 0
+	data = 0;
+	tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+
+	// Loop on 16 DQ
+    for(j = 0; j < 16; j++){
+
+    	// Store initial value of count of the IFSTA register
+    	dq_path = j << 12;
+    	tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &dq_path);
+    	tsc_csr_read(SMEM_DDR3_IFSTA[mem - 1], &cnt_value);
+
+    	init_cnt_value_store[j] = cnt_value & 0xff;
+    	temp_cnt_value_store[j] = init_cnt_value_store[j];
+printf("IFSTA at begining: raw register: 0x%04x stored count: 0x%x \n", cnt_value, init_cnt_value_store[j]);
+    	printf(" DQ[%02d] test   :", j);
+
+		// Reset avg_x, start index, number of test passed "ok" and end value for each DQ
+		avg_x = 0;
+		start = 0;
+		end   = 0;
+		ok    = 0;
+
+		// Add steps by steps for current DQ from initial count value to max
+		for(k = init_cnt_value_store[j]; k < MAX ; k = k + 0x8){
+			// Fill DDR3 with test pattern
+			memcpy(buf_ddr, buf_tx, size);
+			// Get data from DDR3
+			memcpy(buf_rx, buf_ddr, size);
+
+			// Acquire test results in result array corresponding to the current DQ
+			for(m = 0; m < 16; m++){
+				pattern[m * 2]       =  (buf_rx[m] & (0x1 << j)) >>  j;
+				pattern[(m * 2) + 1] =  (buf_rx[m] & (0x1 << (16 + j))) >> (16 + j);
+			}
+
+			// Check data received with reference pattern
+			if (!memcmp(pattern, ref_pattern, 32 * sizeof(int))){
+				printf("  Y");
+				end   = k;
+				NOK   = 0;
+				ok++;
+			}
+			else{
+				printf("  N");
+			}
+
+			// Increment only the tap delay when we are < MAX tap
+			if(k < (MAX - 8)) {
+
+#ifdef PPC
+				if(j < 8){
+					// Compute new count value and write IFSTA
+					data = (temp_cnt_value_store[j] + CURRENT_STEP) << 16;
+					tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+//printf("Current cnt:%x New cnt: %x Computed cnt: %x \n", temp_cnt_value_store[j], (temp_cnt_value_store[j] + CURRENT_STEP), data);
+
+					// Load new count value
+					data = (1 << 31) | (0x1 << (j /*+ 8*/));
+					tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+
+// Check
+//data = j << 12;
+//tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+//tsc_csr_read(SMEM_DDR3_IFSTA[mem - 1], &data);
+//printf("IFSTA after writing new cnt value: %x \n", data);
+
+					// Update new value of count
+					temp_cnt_value_store[j] = temp_cnt_value_store[j] + CURRENT_STEP;
+				}
+				else {
+					// Compute new count value and write IFSTA
+					data = (temp_cnt_value_store[j] + CURRENT_STEP) << 16;
+					tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+//printf("Current cnt:%x New cnt: %x Computed cnt: %x \n", temp_cnt_value_store[j], (temp_cnt_value_store[j] + CURRENT_STEP), data);
+
+					// Load new count value
+					data = (1 << 31) | (0x1 << (j /*- 8*/));
+					tsc_csr_write(SMEM_DDR3_IDEL[mem - 1 ], &data);
+
+// Check
+//data = j << 12;
+//tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+//tsc_csr_read(SMEM_DDR3_IFSTA[mem - 1], &data);
+//printf("IFSTA after writing new cnt value: %x \n", data);
+
+					// Update new value of count
+					temp_cnt_value_store[j] = temp_cnt_value_store[j] + CURRENT_STEP;
+				}
+#else
+				// Compute new count value and write IFSTA
+				data = (temp_cnt_value_store[j] + CURRENT_STEP) << 16;
+				tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+
+				// Load new count value
+				data = (1 << 31) | (0x1 << (j));
+				tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+
+				// Update new value of count
+				temp_cnt_value_store[j] = temp_cnt_value_store[j] + CURRENT_STEP;
+#endif
+			}
+	}
+
+		// If calibration failed set the default count value
+		if (ok == 0){
+			marker = init_cnt_value_store[j];
+		}
+		// Update the new count value with the median value
+		else {
+			// Compute the start window
+			start = (end - (ok * 8)) + 8/*1*/;
+
+			// Compute the average of the window
+			avg_x = (ok * 8) / 2;
+
+			// Compute the new delay marker to apply
+			marker = start + avg_x;
+		}
+
+		// Update the array with the new count value
+		final_cnt_value_store[j] = marker;
+
+		// Trace new delay
+		printf("\n");
+		printf(" Final delay 0x%x:", marker);
+		for(n = init_cnt_value_store[j] ; n < marker; n = n + 8){
+			printf("   ");
+		}
+		printf("  *");
+		printf("\n");
+
+#ifdef PPC
+		if(j < 8){
+			// Compute new count value and write IFSTA
+			data = final_cnt_value_store[j] << 16;
+			tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+
+			// Load new count value
+			data = (1 << 31) | (0x1 << (j /*+ 8*/));
+			tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+
+// Check
+data = (j << 12);
+tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+tsc_csr_read(SMEM_DDR3_IFSTA[mem - 1], &data);
+printf("reread IFSTA after update : %x \n", data);
+
+		}
+		else {
+			// Compute new count value and write IFSTA
+			data = final_cnt_value_store[j] << 16;
+			tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+
+			// Load new count value
+			data = (1 << 31) | (0x1 << (j /*- 8*/));
+			tsc_csr_write(SMEM_DDR3_IDEL[mem - 1 ], &data);
+
+// Check
+data = (j << 12);
+tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+tsc_csr_read(SMEM_DDR3_IFSTA[mem - 1], &data);
+printf("reread IFSTA after update : %x \n", data);
+
+		}
+#else
+		// Compute new count value and write IFSTA
+		data = final_cnt_value_store[j] << 16;
+		tsc_csr_write(SMEM_DDR3_IFSTA[mem - 1], &data);
+
+		// Load new count value
+		data = (1 << 31) | (0x1 << (j));
+		tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+#endif
+
+		// Check status
+		if (ok == 0){
+			NOK = 1;
+			DQ_NOK[j] = 1;
+		}
+		else {
+			DQ_NOK[j] = 0;
+		}
+
+printf(" start: 0x%x, end: 0x%x, ok: %i, avg: 0x%x, marker: 0x%x \n", start, end, ok, avg_x, marker);
+
+		printf("+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------+ \n");
+
+	    // Set IDEL to 0
+		data = 0;
+		tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+    }
+
+    // Execution is finished OK or NOK
+    printf("\n");
+	if (NOK == 1){
+	 	printf("Calibration is not possible, error on line(s) : \n");
+		for (m = 0; m < 16; m++){
+			if (DQ_NOK[m] == 1){
+				printf("DQK[%i] \n", m);
+			}
+		}
+	}
+	else {
+		printf("Calibration done ! \n");
+	}
+
+    // Set IDEL to 0
+	data = 0;
+	tsc_csr_write(SMEM_DDR3_IDEL[mem - 1], &data);
+
+	// Unmap DDR3 memory
+	munmap(buf_ddr, map_win.req.size);
+	tsc_map_free(&map_win);
+
+	// Free buffer
+    free(buf_tx);
+    free(buf_rx);
+
+    return 0;
+}
+
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+// ----------------------------------------------------------------------------------------------------------------------------
+
+// Althea ddr align
+// Align the DDR memory delay
+// When adjust delay, R/W is needed to apply delay
+// There are 32 delays taps 0..31
+// Delay register is wrap-around
+// MAthing DQ <-> DATA
+// DQ[15..8] -> DATA[7..0]
+// DQ[07..0] -> DATA[15..8]
+// ----------------------------------------------------------------------------------
+int ORIGINAL_althea_ddr_idel_calib(int mem){
 	struct tsc_ioctl_map_win map_win;
 	char para_buf[32];
 	int  DQ_NOK[16];
@@ -640,6 +1100,17 @@ int althea_ddr_idel_calib(int mem){
     free(buf_rx);
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
 
 // Main function for Althea command
 // ----------------------------------------------------------------------------------
