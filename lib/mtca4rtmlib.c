10a08577ef32a8cc9e4c0615f1221c2da913e1d6
Xavier Paillard <xavier.paillard@ioxos.ch>
2017-12-01 15:35:19 +0100

    Unify header [XP]

--- lib/mtca4rtmlib.c
+++ lib/mtca4rtmlib.c
@@ -14,13 +14,13 @@
  *----------------------------------------------------------------------------
  *
  *  Copyright Notice
- *  
- *    Copyright and all other rights in this document are reserved by 
- *    IOxOS Technologies SA. This documents contains proprietary information    
- *    and is supplied on express condition that it may not be disclosed, 
+ *
+ *    Copyright and all other rights in this document are reserved by
+ *    IOxOS Technologies SA. This documents contains proprietary information
+ *    and is supplied on express condition that it may not be disclosed,
  *    reproduced in whole or in part, or used for any other purpose other
- *    than that for which it is supplies, without the written consent of  
- *    IOxOS Technologies SA                                                        
+ *    than that for which it is supplies, without the written consent of
+ *    IOxOS Technologies SA
  *
  *=============================< end file header >============================*/
 
