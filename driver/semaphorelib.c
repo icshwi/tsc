4c380fa76ea519e39d3ed2f844a244be09459501
Xavier Paillard <xavier.paillard@ioxos.ch>
2017-12-08 16:16:30 +0100

    Add endianness control [XP]

--- driver/semaphorelib.c
+++ driver/semaphorelib.c
@@ -51,6 +51,8 @@
 #define DBG
 #include "debug.h"
 
+#define PPC 1
+
 /*++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
  * Function name : semaphore_status
  * Prototype     : int
