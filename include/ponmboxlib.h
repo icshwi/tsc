974d1805b899d2d16d5b79ca328877efcc3f5a47
Ralph Hoffmann <ralph.hoffmann@ioxos.ch>
2018-04-05 11:20:31 +0200

    Move private defs/functions of PON mbox from header file back to source file

--- include/ponmboxlib.h
+++ include/ponmboxlib.h
@@ -28,69 +28,13 @@
 #ifndef _H_PONMBOXLIB
 #define _H_PONMBOXLIB
 
-/*
- * Descriptors definitions
- */
-
-#define MAGIC_BYTE_VALUE_VALID               					        0x1F
-#define MAGIC_BYTE_VALUE_TEMPORARY                                      0xAA
-
-#define DESCRIPTOR_TYPE_NONE                                            0x00
-#define DESCRIPTOR_TYPE_MAILBOX_INFO                                    0x01
-#define DESCRIPTOR_TYPE_FIRMWARE_INFO                                   0x02
-#define DESCRIPTOR_TYPE_BOARD_INFO                                      0x03
-#define DESCRIPTOR_TYPE_PRODUCT_INFO                                    0x04
-#define DESCRIPTOR_TYPE_SENSOR_DATA_VALUE                               0x05
-#define DESCRIPTOR_TYPE_SERVICE_REQUEST                                 0x06
-#define DESCRIPTOR_TYPE_RTM_INFO                                        0x07
-
-#define DESCRIPTOR_FORMAT_VERSION_FIRMWARE_INFO                         0x01
-#define DESCRIPTOR_FORMAT_VERSION_BOARD_INFO                            0x01
-#define DESCRIPTOR_FORMAT_VERSION_PRODUCT_INFO                          0x01
-#define DESCRIPTOR_FORMAT_VERSION_SENSOR_DATA_VALUE                     0x01
-#define DESCRIPTOR_FORMAT_VERSION_SERVICE_REQUEST                       0x01
-#define DESCRIPTOR_FORMAT_VERSION_RTM_INFO                              0x01
-
-#define SENSOR_DATA_VALUE_SIZE_8BIT                                     0x01
-#define SENSOR_DATA_VALUE_SIZE_16BIT                                    0x02
-#define SENSOR_DATA_VALUE_SIZE_32BIT                                    0x04
-
-#define SENSOR_VALUE_STATUS_NOT_INITIALIZED                             0x01
-#define SENSOR_VALUE_STATUS_UPDATE_IN_PROGRESS                          0x02
-#define SENSOR_VALUE_STATUS_VALID                                       0x03
-#define SENSOR_VALUE_STATUS_ERROR_UNABLE_TO_UPDATE                      0x04
-
-/*
- * Service requests definitions
- */
-
-#define SERVICE_REQUEST_REQUESTER_ID_OFFSET                             0x00
-#define SERVICE_REQUEST_PROVIDER_ID_OFFSET                              0x01
-#define SERVICE_REQUEST_STATUS_OFFSET                                   0x02
-#define SERVICE_REQUEST_COMPLETION_CODE_OFFSET                          0x03
-#define SERVICE_REQUEST_COMMAND_OFFSET                                  0x04
-#define SERVICE_REQUEST_ARGUMENTS_LENGTH_OFFSET                         0x05
-#define SERVICE_REQUEST_ARGUMENTS_OFFSET                                0x06
-
-#define SERVICE_REQUEST_MMC_ID                                          0x01
-#define SERVICE_REQUEST_CPU_ID                                          0x10
-#define SERVICE_REQUEST_PON_FPGA_ID                                     0x20
-#define SERVICE_REQUEST_CENTRAL_FPGA_ID                                 0x30
-#define SERVICE_REQUEST_RTM_ID                                          0x40
-
-#define SERVICE_REQUEST_STATUS_SUSPENDED                                   1
-#define SERVICE_REQUEST_STATUS_PENDING                                     2
-#define SERVICE_REQUEST_STATUS_PROCESSING                                  3
-#define SERVICE_REQUEST_STATUS_COMPLETED                                   4
-
-#define SERVICE_REQUEST_NORMAL_COMPLETION_CODE                          0x00
-
-#define SERVICE_REQUEST_MAX_ARGUMENTS_LENGTH                               8
 
 #define RTM_STATUS_ABSENT                              0
 #define RTM_STATUS_INCOMPATIBLE                        1
 #define RTM_STATUS_COMPATIBLE_NO_PAYLOAD_POWER         2
 #define RTM_STATUS_COMPATIBLE_HAS_PAYLOAD_POWER        3
+
+#define IOXOS_MANUFACTURER_ID                          0xbe5e
 
 typedef struct mbox_sensor_data_value
 {
@@ -134,20 +78,10 @@ typedef struct
   mbox_sensor_data_value_t *sensors_values;
 } mbox_info_t;
 
-mbox_info_t *alloc_mbox_info(void);
-mbox_info_t *get_mbox_info(void);
 
-int get_mbox_byte(int offset, unsigned char *destination);
-int pop_mbox_byte(int *offset, unsigned char *destination);
-int pop_mbox_short(int *offset, unsigned short *destination);
-int pop_mbox_tribyte(int *offset, unsigned int *destination);
-int pop_mbox_int(int *offset, unsigned int *destination);
-int push_mbox_byte(int *offset, unsigned char byte);
-int get_mbox_sensor_value(mbox_info_t *info, unsigned char *name, int *value, int *timestamp);
-void enable_service_request(mbox_info_t *info, int offset);
-void wait_for_service_request_completion(mbox_info_t *info, int offset);
+mbox_info_t *get_mbox_info(void);
 void free_mbox_info(mbox_info_t *info);
+int get_mbox_sensor_value(mbox_info_t *info, unsigned char *name, int *value, int *timestamp);
 unsigned char send_mbox_service_request(mbox_info_t *info, unsigned char command, unsigned char argc, ...);
-unsigned char pop_mbox_string(int *offset, unsigned char **destination);
 
 #endif
