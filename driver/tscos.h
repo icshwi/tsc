694265b18d8c96b509fc1b8baa24243b3681a9f8
Oliver Talevski <oliver.talevski@esss.se>
2018-10-03 08:40:37 +0200

    Move sched.h and errno.h before uaccess.h
    	In new kernels >= 4.14 sched.h and errno.h are removed from
    	uaccess.h and need to be included by the driver before uaccess.h.

--- driver/tscos.h
+++ driver/tscos.h
@@ -44,8 +44,9 @@
  *
  *=============================< end file header >============================*/
 
-#include <asm/uaccess.h>         /* copy_to_user and copy_from_user */
+#include <linux/sched.h>
 #include <linux/errno.h>         /* error codes                     */
+#include <asm/uaccess.h>         /* copy_to_user and copy_from_user */
 #include <linux/slab.h>          /* kmalloc and kfree               */
 #include <linux/cdev.h>          /* struct cdev                     */
 #include <linux/pci.h>
@@ -54,4 +55,3 @@
 #include <linux/interrupt.h>
 #include <linux/poll.h>
 #include <linux/init.h>          // modules
-#include <linux/sched.h>         // module
