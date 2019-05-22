db91d5367355e5ca0707c1ed54c83d06a7ed05d3
Oliver Talevski <oliver.talevski@esss.se>
2019-05-22 09:23:58 +0200

    Update driver version to 3.5.0

--- driver/tscdrvr.c
+++ driver/tscdrvr.c
@@ -51,7 +51,7 @@
 #define DBGno
 #include "debug.h"
 
-#define DRIVER_VERSION "3.4.1"
+#define DRIVER_VERSION "3.5.0"
 
 LIST_HEAD(tsc_devlist);             /**< List of device context structs for available devices. */
 DEFINE_MUTEX(tsc_devlist_lock);     /**< Lock that serializes access to the list of available devices. */
